5c3c9fff1c4e05bead3ebdf24483e977165a4c98
mgerashchenko <mgerashchenko@gmail.com>
2020-08-11 23:17:10 -0400

    Add grid template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  weeks = [];
+
+  age = 200;
+
+  ngOnInit(){
+    for(let i=1; i<=1000; i++) {
+      this.weeks.push(i);
+    }
+  }
 }
