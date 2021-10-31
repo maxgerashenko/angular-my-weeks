22ac3b3b913e50188d8e41efeef766cc33c90f03
mgerashchenko <mgerashchenko@gmail.com>
2021-10-30 22:31:58 -0400

    1 file modified, 3 added

--- src/input/input.module.ts
+++ src/input/input.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './input.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [],
+  declarations: [InputComponent],
+  exports: [InputComponent],
 })
 export class InputModule {}
