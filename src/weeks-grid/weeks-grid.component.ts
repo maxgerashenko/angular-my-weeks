46b4c4725120cb4488bb4db6ec2f519b8dd38403
mgerashchenko <mgerashchenko@gmail.com>
2021-10-30 22:14:26 -0400

    Update grid css

--- src/weeks-grid/weeks-grid.component.ts
+++ src/weeks-grid/weeks-grid.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+const AVERAGE_AGE = 85;
+const WEEKS_IN_YEAR = 52;
+
 @Component({
   selector: 'app-weeks-grid',
   templateUrl: './weeks-grid.component.html',
@@ -7,13 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeeksGridComponent implements OnInit {
   weeks = [];
-
-  age = 10;
+  totalAge = AVERAGE_AGE * WEEKS_IN_YEAR;
+  age = 30 * WEEKS_IN_YEAR;
 
   constructor() {}
 
   ngOnInit() {
-    for (let i = 1; i <= 1000; i++) {
+    for (let i = 1; i <= this.totalAge; i++) {
       this.weeks.push(i);
     }
   }
